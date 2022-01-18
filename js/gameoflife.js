6d88f7b01ddddae6b555ee94fca522eff33b1632
mabdull5 <muhammad.a.abdullah@exxonmobil.com>
2022-01-18 11:34:57 +0100

    refactored: getNeighborsOf

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -46,14 +46,16 @@ const printCells = (state) => {
 };
 
 const getNeighborsOf = ([x, y]) => {
-  [x - 1, y + 1],
+  [
+    [x - 1, y + 1],
     [x, y + 1],
     [x + 1, y + 1],
     [x - 1, y],
     [x + 1, y],
     [x - 1, y - 1],
     [x, y - 1],
-    [x + 1, y - 1];
+    [x + 1, y - 1],
+  ];
 };
 
 const getLivingNeighbors = (cell, state) => {};
